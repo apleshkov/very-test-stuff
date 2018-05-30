dfdef70d401e32c69a7d86efa7a9770f633f7249
apleshkov <andrew.pleshkov@gmail.com>
2018-05-31 01:50:44 +0300

    dev

---
+++ Saber/Tests/SaberTests/Generator/TypeResolver/BoundTypeResolverTests.swift
@@ -0,0 +1,15 @@
+//
+//  BoundTypeResolverTests.swift
+//  SwordGeneratorTests
+//
+//  Created by andrey.pleshkov on 11/05/2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import XCTest
+@testable import Saber
+
+class BoundTypeResolverTests: XCTestCase {
+
+    
+}
